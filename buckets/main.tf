568d7104a883e6f48bbd9e9b946d71a27d375ae9
isaacmg <igodfried@isaac26.com>
2023-12-12 04:14:39 -0500

    fixing stuff 4

--- buckets/main.tf
+++ buckets/main.tf
@@ -2,7 +2,5 @@ resource "google_storage_bucket" "flow_data" {
   name          = "flow_hydro_2_data"
   location      = "us-west2"
   force_destroy = true
-
   uniform_bucket_level_access = true
-
 }
