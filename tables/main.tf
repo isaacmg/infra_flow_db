754eda9620e9b5237643027109015b7af43dbfc5
isaacmg <igodfried@isaac26.com>
2024-01-13 20:26:29 -0500

    Revert "changing fields and editing main 3"
    This reverts commit d16b793c344e9a275ff7e092dcbf7f4a636eac78.

--- tables/main.tf
+++ tables/main.tf
@@ -3,6 +3,7 @@ resource "google_bigquery_dataset" "hydronet" {
   friendly_name               = "test"
   description                 = "This is a test description"
   location                    = "EU"
+  default_table_expiration_ms = 3600000
   project = "hydro-earthnet-db"
 
   labels = {
